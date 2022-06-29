3962d1e3d0f348361ccabfae31d6f3bd4be1361f
jamin0ne <56770256+jamin0ne@users.noreply.github.com>
2022-06-29 14:41:25 +0300

    setup github to firebase depolyment test 2

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -17,7 +17,7 @@ export default function AboutUsPage() {
       </p>
       <div>
         To see list of our products
-        <Button as={Link} to="/Productspage" color="blue">
+        <Button as={Link} to="/Productspage" color="red">
           Click Me
         </Button>
       </div>
