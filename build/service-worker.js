f8c39bbfc0134abc8acff701b19be30662c70f50
jamin0ne <56770256+jamin0ne@users.noreply.github.com>
2022-07-14 02:45:31 +0300

    using original build

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c04448116eb1f321cba31fda6b77fbb7.js"
+  "/precache-manifest.b05a39c70b4999e7fc8fca5b0a984bb9.js"
 );
 
 workbox.clientsClaim();
