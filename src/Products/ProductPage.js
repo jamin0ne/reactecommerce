942f38148c2e7bdd1121cb5e77a5083e2b50f5cd
jamin0ne <56770256+jamin0ne@users.noreply.github.com>
2023-01-30 12:46:14 +0000

    completed product listing 2

--- src/Products/ProductPage.js
+++ src/Products/ProductPage.js
@@ -20,123 +20,9 @@ import {Db} from '../settingsAndConfig/DBconfig/FbConfig'
 		price: 15,
 		category:"tuber vegetable",
 		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  },{
-		title:"Yam",
-		image:"https://cdn11.bigcommerce.com/s-58uvul1jf2/images/stencil/1280x1280/products/362/832/Fresh_Yam_2__47180.1619302542.1280.1280__50156.1656418056.jpg?c=1",
-		price: 15,
-		category:"tuber vegetable",
-		short_description: "Tuber of yam, from west african region"
-	  }]);
+	  },]);
 	   
- /*  const  getProductData = async()=>{
+  const  getProductData = async()=>{
 		     const DataArrangeMentArray = []
 		const getproduct = await getDocs(collection(Db, "Products"));
 		   getproduct.forEach(product=>{
@@ -149,7 +35,7 @@ import {Db} from '../settingsAndConfig/DBconfig/FbConfig'
 		   console.log(DataArrangeMentArray)
 	   }
 	   
-	      useEffect(()=>{getProductData()})*/
+	      useEffect(()=>{getProductData()})
 	  
           return ( <div class="marketplace-container">
 			<span class="marketplace-text"><span>Products</span></span>
